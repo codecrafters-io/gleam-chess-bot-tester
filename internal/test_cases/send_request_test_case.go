0398fcdc91ac24e115d5f14145586c48f30f33d4
Ryan Gang <ryan-gg@outlook.com>
2025-03-18 10:52:02 +0530

    refactor: update SendRequestTestCase to use assertion interface and remove unused functions

--- internal/test_cases/send_request_test_case.go
+++ internal/test_cases/send_request_test_case.go
@@ -2,10 +2,10 @@ package test_cases
 
 import (
 	"fmt"
-	"io"
 	"net/http"
 	"time"
 
+	"github.com/codecrafters-io/gleam-chess-bot-tester/internal/assertions"
 	"github.com/codecrafters-io/tester-utils/logger"
 	"github.com/codecrafters-io/tester-utils/test_case_harness"
 )
@@ -17,7 +17,7 @@ const ADDRESS = HOST + ":" + PORT + ENDPOINT
 
 type SendRequestTestCase struct {
 	Request   *http.Request
-	Assertion []func(*http.Response) error
+	Assertion []assertions.Assertion
 }
 
 func (tc *SendRequestTestCase) Run(harness *test_case_harness.TestCaseHarness, logger *logger.Logger) error {
@@ -25,9 +25,6 @@ func (tc *SendRequestTestCase) Run(harness *test_case_harness.TestCaseHarness, l
 	client := &http.Client{
 		Timeout: 5 * time.Second,
 	}
-
-	// // Wait a bit for the server to start
-	// time.Sleep(1 * time.Second)
 
 	// Make the request
 	logger.Debugf("Making HTTP request to %s", tc.Request.URL)
@@ -40,33 +37,12 @@ func (tc *SendRequestTestCase) Run(harness *test_case_harness.TestCaseHarness, l
 	// Run the assertion
 	if tc.Assertion != nil {
 		for _, assertion := range tc.Assertion {
-			if err := assertion(response); err != nil {
+			if err := assertion.Run(response); err != nil {
 				return fmt.Errorf("assertion failed: %v", err)
 			}
 		}
 	}
 
 	logger.Successf("Request completed successfully")
-	return nil
-}
-
-func StatusCodeIs200(response *http.Response) error {
-	if response.StatusCode != http.StatusOK {
-		return fmt.Errorf("expected status code 200, got %d", response.StatusCode)
-	}
-
-	return nil
-}
-
-func BodyIs(response *http.Response, expectedBody string) error {
-	body, err := io.ReadAll(response.Body)
-	if err != nil {
-		return fmt.Errorf("failed to read response body: %v", err)
-	}
-
-	if string(body) != expectedBody {
-		return fmt.Errorf("expected body %s, got %s", expectedBody, string(body))
-	}
-
 	return nil
 }
