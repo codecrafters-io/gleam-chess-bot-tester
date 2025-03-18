112d20b22533e712243e115dc06eb96986200f10
Ryan Gang <ryan-gg@outlook.com>
2025-03-18 11:30:44 +0530

    feat: implement test3 function to execute chess bot and validate move requests with FEN and turn information

--- internal/stage_2.go
+++ internal/stage_2.go
@@ -19,6 +19,7 @@ func test2(stageHarness *test_case_harness.TestCaseHarness) error {
 	logger := stageHarness.Logger
 
 	FEN := "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
+
 	request, err := http.NewRequest("POST", test_cases.ADDRESS, bytes.NewBuffer([]byte(`{"fen": "`+FEN+`", "turn": "white", "failed_moves": []}`)))
 	if err != nil {
 		return err
@@ -26,7 +27,7 @@ func test2(stageHarness *test_case_harness.TestCaseHarness) error {
 
 	test_case := test_cases.SendRequestTestCase{
 		Request:   request,
-		Assertion: []assertions.Assertion{&assertions.StatusCodeAssertion{ExpectedStatusCode: 200}, &assertions.ValidMoveAssertion{FEN: FEN}},
+		Assertion: []assertions.Assertion{&assertions.StatusCodeAssertion{ExpectedStatusCode: 200}},
 	}
 
 	return test_case.Run(stageHarness, logger)
--- internal/stage_3.go
+++ internal/stage_3.go
@@ -1,9 +1,33 @@
 package internal
 
 import (
+	"bytes"
+	"net/http"
+
+	"github.com/codecrafters-io/gleam-chess-bot-tester/internal/assertions"
+	chess_bot_executable "github.com/codecrafters-io/gleam-chess-bot-tester/internal/chess-bot-executable"
+	"github.com/codecrafters-io/gleam-chess-bot-tester/internal/test_cases"
 	"github.com/codecrafters-io/tester-utils/test_case_harness"
 )
 
 func test3(stageHarness *test_case_harness.TestCaseHarness) error {
-	return nil
+	b := chess_bot_executable.NewChessBotExecutable(stageHarness)
+	if err := b.Run(); err != nil {
+		return err
+	}
+
+	logger := stageHarness.Logger
+
+	FEN := "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
+	request, err := http.NewRequest("POST", test_cases.ADDRESS, bytes.NewBuffer([]byte(`{"fen": "`+FEN+`", "turn": "white", "failed_moves": []}`)))
+	if err != nil {
+		return err
+	}
+
+	test_case := test_cases.SendRequestTestCase{
+		Request:   request,
+		Assertion: []assertions.Assertion{&assertions.StatusCodeAssertion{ExpectedStatusCode: 200}, &assertions.ValidMoveAssertion{FEN: FEN}},
+	}
+
+	return test_case.Run(stageHarness, logger)
 }
