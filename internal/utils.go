503b18c40f0e92334477c2114ada6f1bafaa4bb2
Ryan Gang <ryan-gg@outlook.com>
2025-03-18 11:29:39 +0530

    feat: implement makeMove function to handle chess move requests with FEN and turn information

--- internal/utils.go
+++ internal/utils.go
@@ -1,29 +1,70 @@
-func moveIsValid(fenStr string, move string) bool {
+package internal
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"time"
+
+	"github.com/corentings/chess"
+)
+
+func makeMove(fenStr string) (string, error) {
 	fen, err := chess.FEN(fenStr)
 	if err != nil {
-		return false
+		return "", err
 	}
 
 	game := chess.NewGame(fen)
 
-	// Try to make the move
-	err = game.MoveStr(move)
-	if err != nil {
-		// If move is invalid, try UCI notation
-		moveUCI, err := chess.UCINotation{}.Decode(game.Position(), move)
-		if err != nil {
-			return false
-		}
-
-		// Check if the UCI move is in the list of valid moves
-		validMoves := game.ValidMoves()
-		for _, validMove := range validMoves {
-			if moveUCI.String() == validMove.String() {
-				return true
-			}
-		}
-		return false
+	// Determine turn color
+	turn := "white"
+	if game.Position().Turn() == chess.Black {
+		turn = "black"
 	}
 
-	return true
+	// Prepare request body
+	type requestBody struct {
+		Fen         string   `json:"fen"`
+		Turn        string   `json:"turn"`
+		FailedMoves []string `json:"failed_moves"`
+	}
+
+	body := requestBody{
+		Fen:         fenStr,
+		Turn:        turn,
+		FailedMoves: []string{},
+	}
+
+	jsonBody, err := json.Marshal(body)
+	if err != nil {
+		return "", fmt.Errorf("error marshaling request body: %v", err)
+	}
+
+	client := &http.Client{
+		Timeout: 5 * time.Second,
+	}
+
+	resp, err := client.Post(
+		"http://localhost:8000/move",
+		"application/json",
+		bytes.NewBuffer(jsonBody),
+	)
+	if err != nil {
+		return "", fmt.Errorf("error making request: %v", err)
+	}
+	defer resp.Body.Close()
+
+	responseBody, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return "", fmt.Errorf("error reading response: %v", err)
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		return "", fmt.Errorf("failed to make move, got status %d: %s", resp.StatusCode, string(responseBody))
+	}
+
+	return string(responseBody), nil
 }
