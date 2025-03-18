5aa5779276d8e5a775f1cb3959f733ff7bdaa744
Ryan Gang <ryan-gg@outlook.com>
2025-03-18 11:42:13 +0530

    feat: enhance GetMoveTestCase to validate generated moves and include turn information in requests

--- internal/test_cases/get_move_test_case.go
+++ internal/test_cases/get_move_test_case.go
@@ -6,21 +6,40 @@ import (
 
 	"github.com/codecrafters-io/gleam-chess-bot-tester/internal/assertions"
 	"github.com/codecrafters-io/tester-utils/test_case_harness"
+	"github.com/corentings/chess"
 )
 
 type GetMoveTestCase struct {
-	FEN string
+	FEN                        string
+	AssertGeneratedMoveIsValid bool
+}
+
+func getTurn(fenStr string) string {
+	fen, err := chess.FEN(fenStr)
+	if err != nil {
+		panic("Failed to parse FEN: " + err.Error())
+	}
+
+	game := chess.NewGame(fen)
+	return game.Position().Turn().String()
 }
 
 func (tc *GetMoveTestCase) Run(stageHarness *test_case_harness.TestCaseHarness) error {
-	request, err := http.NewRequest("POST", ADDRESS, bytes.NewBuffer([]byte(`{"fen": "`+tc.FEN+`", "turn": "white", "failed_moves": []}`)))
+	request, err := http.NewRequest("POST", ADDRESS, bytes.NewBuffer([]byte(`{"fen": "`+tc.FEN+`", "turn": "`+getTurn(tc.FEN)+`", "failed_moves": []}`)))
 	if err != nil {
 		return err
 	}
 
+	request.Header.Set("Content-Type", "application/json")
+
+	allAssertions := []assertions.Assertion{&assertions.StatusCodeAssertion{ExpectedStatusCode: 200}}
+	if tc.AssertGeneratedMoveIsValid {
+		allAssertions = append(allAssertions, &assertions.ValidMoveAssertion{FEN: tc.FEN})
+	}
+
 	test_case := SendRequestTestCase{
 		Request:   request,
-		Assertion: []assertions.Assertion{&assertions.StatusCodeAssertion{ExpectedStatusCode: 200}, &assertions.ValidMoveAssertion{FEN: tc.FEN}},
+		Assertion: allAssertions,
 	}
 
 	return test_case.Run(stageHarness, stageHarness.Logger)
