68b592554839f526eb68f1b48c9716439d433380
Ryan Gang <ryan-gg@outlook.com>
2025-03-18 11:28:42 +0530

    feat: add failure_bot scenario with configuration and main application for handling move requests

--- internal/test_helpers/scenarios/failure_bot/app/main.go
+++ internal/test_helpers/scenarios/failure_bot/app/main.go
@@ -5,7 +5,7 @@ import "net/http"
 func main() {
 	http.HandleFunc("/move", func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusOK)
-		w.Write([]byte("e2e4"))
+		w.Write([]byte("e9e10"))
 	})
 
 	http.ListenAndServe(":8000", nil)
