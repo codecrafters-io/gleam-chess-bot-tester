66f0a974a0cf3a54194953b1716ff84243d77631
Ryan Gang <ryan-gg@outlook.com>
2025-03-19 16:22:01 +0530

    feat: enhance anti-cheat functionality by adding whitelisted dependencies check and FFI validation

--- internal/anti_cheat.go
+++ internal/anti_cheat.go
@@ -4,12 +4,34 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
+	"slices"
+	"strings"
 
 	"github.com/BurntSushi/toml"
 	chess_bot_executable "github.com/codecrafters-io/gleam-chess-bot-tester/internal/chess-bot-executable"
 	loggerUtils "github.com/codecrafters-io/tester-utils/logger"
 	"github.com/codecrafters-io/tester-utils/test_case_harness"
 )
+
+var WHITELISTED_DEPENDENCIES = []string{
+	"gleam_stdlib",
+	"gleam_http",
+	"gleam_json",
+	"gleam_time",
+	"gleam_community_maths",
+	"flash",
+	"iv",
+	"glearray",
+	"snag",
+	"birl",
+	"gtempo",
+	"gleam_erlang",
+	"gleam_otp",
+	"mist",
+	"wisp",
+	"gleam_javascript",
+	"glen",
+}
 
 func antiCheatExecute(stageHarness *test_case_harness.TestCaseHarness) error {
 	logger := stageHarness.Logger
@@ -45,26 +67,61 @@ func antiCheatDeps(stageHarness *test_case_harness.TestCaseHarness) error {
 	}
 
 	var gleamTomlConfig struct {
-		Dependencies []struct {
-			Name string
-			Path string
-		}
+		Dependencies map[string]string `toml:"dependencies"`
 	}
 	if err := toml.Unmarshal(gleamToml, &gleamTomlConfig); err != nil {
 		logger.Criticalf("CodeCrafters internal error. Error unmarshalling gleam.toml: %v", err)
 		logger.Criticalf("Try again? Please contact us at hello@codecrafters.io if this persists.")
-		return fmt.Errorf("anti-cheat (ac1) failed")
+		return fmt.Errorf("anti-cheat (ac2) failed")
 	}
 
-	fmt.Println(gleamTomlConfig.Dependencies)
+	deps_3p := make([]string, 0, len(gleamTomlConfig.Dependencies))
+	for k := range gleamTomlConfig.Dependencies {
+		deps_3p = append(deps_3p, k)
+	}
 
-	for _, dependency := range gleamTomlConfig.Dependencies {
-		if dependency.Name == "gleam_stdlib" {
-			stdlibPath := filepath.Join(executableDir, dependency.Path)
-			if _, err := os.Stat(stdlibPath); os.IsNotExist(err) {
-				logger.Criticalf("CodeCrafters internal error. gleam_stdlib not found at %s", stdlibPath)
+	for _, dep := range deps_3p {
+		if !slices.Contains(WHITELISTED_DEPENDENCIES, dep) {
+			logger.Criticalf("CodeCrafters internal error. Dependency %s is not allowed", dep)
+			logger.Criticalf("Try again? Please contact us at hello@codecrafters.io if this persists.")
+			return fmt.Errorf("anti-cheat (ac1) failed")
+		}
+	}
+
+	return nil
+}
+
+func antiCheatFFI(stageHarness *test_case_harness.TestCaseHarness) error {
+	logger := loggerUtils.GetQuietLogger("")
+
+	b := chess_bot_executable.NewChessBotExecutable(stageHarness)
+	executableDir := b.GetExecutableDirectory()
+
+	var allFiles []string
+	err := filepath.Walk(executableDir, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		if !info.IsDir() {
+			allFiles = append(allFiles, path)
+		}
+		return nil
+	})
+
+	if err != nil {
+		return fmt.Errorf("error walking directory: %v", err)
+	}
+
+	for _, file := range allFiles {
+		if strings.HasSuffix(file, ".gleam") {
+			content, err := os.ReadFile(file)
+			if err != nil {
+				return fmt.Errorf("error reading file: %v", err)
+			}
+			if strings.Contains(string(content), "@external") {
+				logger.Criticalf("CodeCrafters internal error. External FFI functions are not allowed.\nFile: %s", file)
 				logger.Criticalf("Try again? Please contact us at hello@codecrafters.io if this persists.")
-				return fmt.Errorf("anti-cheat (ac1) failed")
+				return fmt.Errorf("anti-cheat (ac3) failed")
 			}
 		}
 	}
