0a9955179e8b79e45096465d704212b3b6d41c47
Ryan Gang <ryan-gg@outlook.com>
2025-03-19 15:24:06 +0530

    refactor: simplify HTTP client creation and add duration check for request completion in SendRequestTestCase

--- internal/test_cases/send_request_test_case.go
+++ internal/test_cases/send_request_test_case.go
@@ -21,14 +21,13 @@ type SendRequestTestCase struct {
 }
 
 func (tc *SendRequestTestCase) Run(logger *logger.Logger) error {
-	// Create HTTP client with timeout
-	client := &http.Client{
-		Timeout: 5 * time.Second,
-	}
+	client := &http.Client{}
 
 	// sleep for 1 second
 	// TODO: Remove this once we've confirmed that the server is running
 	time.Sleep(1 * time.Second)
+
+	startTime := time.Now()
 
 	// Make the request
 	logger.Debugf("Making HTTP request to %s", tc.Request.URL)
@@ -37,6 +36,14 @@ func (tc *SendRequestTestCase) Run(logger *logger.Logger) error {
 		return fmt.Errorf("failed to make request: %v", err)
 	}
 	defer response.Body.Close()
+
+	// Create Response struct set duration on it
+	// Use that for all assertions
+	duration := time.Since(startTime)
+	if duration > 5*time.Second {
+		return fmt.Errorf("request took too long to complete: %s", duration)
+	}
+	logger.Debugf("Request completed in %s", duration)
 
 	// Run the assertion
 	if tc.Assertion != nil {
