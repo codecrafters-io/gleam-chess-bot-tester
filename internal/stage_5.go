bb1a3ee09489384104b829cef882da660110f12f
Ryan Gang <ryan-gg@outlook.com>
2025-04-15 12:53:55 +0530

    feat: add entry.MD file checking functionality to stage5

---
+++ internal/stage_5.go
@@ -0,0 +1,30 @@
+package internal
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+
+	chess_bot_executable "github.com/codecrafters-io/gleam-chess-bot-tester/internal/chess-bot-executable"
+	"github.com/codecrafters-io/tester-utils/test_case_harness"
+)
+
+func test5(stageHarness *test_case_harness.TestCaseHarness) error {
+	b := chess_bot_executable.NewChessBotExecutable(stageHarness)
+	if err := b.Run(); err != nil {
+		return err
+	}
+
+	logger := stageHarness.Logger
+
+	files, err := listFilesInExecutableDir(stageHarness)
+	if err != nil {
+		return err
+	}
+	if !checkForEntryMd(files) {
+		return fmt.Errorf("ENTRY.md not found")
+	}
+	logger.Successf("ENTRY.md file found! You're all set to participate in the Gleam Chess Tournament.")
+
+	return nil
+}
