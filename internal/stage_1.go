f50bef226c09df6f6070237e2b9a90e4caeaf268
Ryan Gang <ryan-gg@outlook.com>
2025-04-15 12:54:42 +0530

    feat: add file existence checks for ENTRY.md OR example.ENTRY.md in stage 1

--- internal/stage_1.go
+++ internal/stage_1.go
@@ -1,6 +1,8 @@
 package internal
 
 import (
+	"fmt"
+
 	chess_bot_executable "github.com/codecrafters-io/gleam-chess-bot-tester/internal/chess-bot-executable"
 	"github.com/codecrafters-io/gleam-chess-bot-tester/internal/test_cases"
 	"github.com/codecrafters-io/tester-utils/test_case_harness"
@@ -13,6 +15,14 @@ func test1(stageHarness *test_case_harness.TestCaseHarness) error {
 	}
 
 	logger := stageHarness.Logger
+
+	files, err := listFilesInExecutableDir(stageHarness)
+	if err != nil {
+		return err
+	}
+	if !checkForEitherExampleOrEntryMd(files) {
+		return fmt.Errorf("Expected to find either ENTRY.md or example.ENTRY.md in the executable directory, but found neither")
+	}
 
 	// Opening position
 	FEN := "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
