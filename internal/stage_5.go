c546d3417b80c14dfd019486c28a4d902357ac9a
Ryan Gang <ryan-gg@outlook.com>
2025-04-15 13:10:17 +0530

    fix: improve error message for missing ENTRY.md in stage 5

--- internal/stage_5.go
+++ internal/stage_5.go
@@ -20,7 +20,7 @@ func test5(stageHarness *test_case_harness.TestCaseHarness) error {
 		return err
 	}
 	if !checkForEntryMd(files) {
-		return fmt.Errorf("ENTRY.md not found")
+		return fmt.Errorf("ENTRY.md file not found, please make sure you rename the example.ENTRY.md file to ENTRY.md, and fill in the required fields to participate in the Gleam Chess Tournament.")
 	}
 	logger.Successf("ENTRY.md file found! You're all set to participate in the Gleam Chess Tournament.")
 
