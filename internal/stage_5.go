4adcaa2dd7dd4cc73a346285a9c1124ece380efa
Ryan Gang <ryan-gg@outlook.com>
2025-04-15 13:04:57 +0530

    feat: move file existence check functions to utils and clean up stage 5

--- internal/stage_5.go
+++ internal/stage_5.go
@@ -2,8 +2,6 @@ package internal
 
 import (
 	"fmt"
-	"os"
-	"path/filepath"
 
 	chess_bot_executable "github.com/codecrafters-io/gleam-chess-bot-tester/internal/chess-bot-executable"
 	"github.com/codecrafters-io/tester-utils/test_case_harness"
@@ -27,41 +25,4 @@ func test5(stageHarness *test_case_harness.TestCaseHarness) error {
 	logger.Successf("ENTRY.md file found! You're all set to participate in the Gleam Chess Tournament.")
 
 	return nil
-}
-
-func checkForEntryMd(files []os.DirEntry) bool {
-	return checkForFile(files, "ENTRY.md")
-}
-
-func checkForExampleEntryMd(files []os.DirEntry) bool {
-	return checkForFile(files, "example.ENTRY.md")
-}
-
-func checkForEitherExampleOrEntryMd(files []os.DirEntry) bool {
-	return checkForExampleEntryMd(files) || checkForEntryMd(files)
-}
-
-func listFilesInExecutableDir(stageHarness *test_case_harness.TestCaseHarness) ([]os.DirEntry, error) {
-	executablePath := stageHarness.Executable.Path
-	executableDir := filepath.Dir(executablePath)
-
-	files, err := os.ReadDir(executableDir)
-	if err != nil {
-		return nil, err
-	}
-
-	return files, nil
-}
-
-func checkForFile(files []os.DirEntry, fileName string) bool {
-	if len(files) == 0 {
-		return false
-	}
-
-	for _, file := range files {
-		if file.Name() == fileName {
-			return true
-		}
-	}
-	return false
 }
--- internal/utils.go
+++ internal/utils.go
@@ -6,8 +6,11 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"os"
+	"path/filepath"
 	"time"
 
+	"github.com/codecrafters-io/tester-utils/test_case_harness"
 	"github.com/corentings/chess"
 )
 
@@ -76,4 +79,43 @@ func checkFEN(FEN string) bool {
 		_, err = chess.FEN(FEN)
 	}
 	return err == nil
+}
+
+// File utils to check for the existence of entry files in the repo
+
+func checkForEntryMd(files []os.DirEntry) bool {
+	return checkForFile(files, "ENTRY.md")
+}
+
+func checkForExampleEntryMd(files []os.DirEntry) bool {
+	return checkForFile(files, "example.ENTRY.md")
+}
+
+func checkForEitherExampleOrEntryMd(files []os.DirEntry) bool {
+	return checkForExampleEntryMd(files) || checkForEntryMd(files)
+}
+
+func listFilesInExecutableDir(stageHarness *test_case_harness.TestCaseHarness) ([]os.DirEntry, error) {
+	executablePath := stageHarness.Executable.Path
+	executableDir := filepath.Dir(executablePath)
+
+	files, err := os.ReadDir(executableDir)
+	if err != nil {
+		return nil, err
+	}
+
+	return files, nil
+}
+
+func checkForFile(files []os.DirEntry, fileName string) bool {
+	if len(files) == 0 {
+		return false
+	}
+
+	for _, file := range files {
+		if file.Name() == fileName {
+			return true
+		}
+	}
+	return false
 }
